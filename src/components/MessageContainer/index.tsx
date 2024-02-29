6f87e2e915b55567ce31ef9a4d6a8824d8456f87
itsjithinv <jvijayan@ischoolconnect.com>
2024-02-29 16:13:47 +0530

    fix: accessibility issues

--- src/components/MessageContainer/index.tsx
+++ src/components/MessageContainer/index.tsx
@@ -103,7 +103,13 @@ export const MessageContainer = React.forwardRef<MessageContainerHandle, Message
           loadMoreText={loadMoreText}
           ref={scroller}
         >
-          <div className="MessageList">
+          <div
+            className="MessageList"
+            role="log"
+            aria-live="polite"
+            aria-relevant="text"
+            aria-atomic="false"
+          >
             {messages.map((msg) => (
               <Message {...msg} renderMessageContent={renderMessageContent} key={msg._id} />
             ))}
